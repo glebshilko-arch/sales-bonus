2dba280ef459220c498079071ae4a062ebe62773
Shilko Gleb <gleb.shilko@gmail.com>
2026-06-13 19:03:20 +0300

    redact10

--- src/main.js
+++ src/main.js
@@ -89,9 +89,9 @@ function analyzeSalesData(data, options) {
              if (!product) return; 
             const cost = product.purchase_price * item.quantity;
             const revenue = calculateRevenue(item, product);
-            seller.revenue = +(seller.revenue + revenue).toFixed(4);
+            seller.revenue += revenue;
             const profit = revenue - cost;
-            seller.profit = +(seller.profit + profit).toFixed(4);
+            seller.profit += profit;
 
             if (!seller.products_sold[item.sku]) {
                 seller.products_sold[item.sku] = 0;
